554ea26e4ac1d26847152c8c227e397a7de6fe3a
HaithamTamer <73913129+HaithamTamer@users.noreply.github.com>
2020-11-08 17:31:55 +0200

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#include <stdio.h>
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    printf("ASCII value of %c = %d", c, c);
+
+    return 0;
+}
